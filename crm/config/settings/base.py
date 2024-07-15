953b027bb9196822721d13dc726dfd9c32458892
Dmytro Masalov <dmitriymasalov5@gmail.com>
2024-07-15 14:06:32 +0300

    feat: add 'customers' app
    Add a new application to display customer information

--- crm/config/settings/base.py
+++ crm/config/settings/base.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    "crm.customers",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
